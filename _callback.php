f3c87bd33716271537f17e9af98cedd4c209db15
Max <github@jacquet.me>
2017-06-12 09:27:06 +0200

    env var for experience callback

--- _callback.php
+++ _callback.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta name="salesforce-community" content="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>">
-	<meta name="salesforce-mode" content="modal-callback">
+	<meta name="salesforce-mode" content="<?php echo getenv('WIDGET_EXPERIENCE');?>-callback">
 	<meta name="salesforce-save-access-token" content="true">
 	<meta name="salesforce-allowed-domains" content="*.herokuapp.com">
 	<script src="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/resource/salesforce_login_widget_js" async defer></script>
